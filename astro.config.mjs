789b40ddb5dc9b0947ca81abed472cd486fecdba
1321tremblay <oliviertremblay13@gmail.com>
2024-04-26 14:54:41 -0400

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://eveastro.netlify.app",
+  integrations: [tailwind(), icon(), sitemap()]
+});
